810d52be632e284f588b5b112b69e5f6d8831094
Gonzalez <gonzalezlouis1981@gmail.com>
2024-03-11 17:04:26 +0100

    WIP refacoring task

--- src/Controllers/TaskController.php
+++ src/Controllers/TaskController.php
@@ -48,32 +48,11 @@ class TaskController
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 
-                    // Se connecter à la base de données
-            $pdo = new Database(
-                                    "127.0.0.1",
-                                    "todolist_php",
-                                    "3306",
-                                    "root",
-                                    ""
-                                );
-
-            // Vérification des champs qui ont update dans le formulaire
-            if(isset($_POST['title']) && isset($_POST['description']) && isset($_POST['duration']) && isset($_POST['status'])
-            && !empty($_POST['title']) && !empty($_POST['description']) && !empty($_POST['duration']) && !empty($_POST['status'])) 
-                {
-                    $title= Utils::cleaner($_POST['title']);
-                    $description = Utils::cleaner($_POST['description']);
-                    $duration = Utils::cleaner($_POST['duration']);
-                    $status = Utils::cleaner($_POST['status']);
-
-                    // Insertion des champs dans la base de données
-                    $newTask = $pdo->query("    INSERT INTO task 
-                                                (title, description, duration, status) 
-                                                VALUES ( '$title', '$description', '$duration', '$status')");
-                    // Redirection vers le tableau des tâches
-                    header('Location: /formation_php/todolist_php/public/task');
-                }
-        }
+                $taskRepository = new TaskRepository();
+                $taskRepository->new();
+                // Redirection vers le tableau des tâches
+                header('Location: /formation_php/todolist_php/public/task');
+            };
         // Rendre une vue
         echo $twig->render('tasknewpage.twig', [
                                                 'text' => $text,
@@ -146,15 +125,8 @@ class TaskController
         // Initialiser twig 
         $twig = new Environment($loader);
 
-        // la correspondance de l'id souhaite via une requete sql
-        // Se connecter à la base de données
-        $pdo = new Database(
-            "127.0.0.1",
-            "todolist_php",
-            "3306",
-            "root",
-            ""
-        );
+        $taskRepository = new TaskRepository();
+        $task = $taskRepository->show($id);
 
         ////////////// Méthode 1 ////////////////////////////////////////////////////////////////
         // $tasks = $pdo->selectAll("SELECT * FROM task ");
@@ -171,8 +143,7 @@ class TaskController
         //                                         ]);
 
         /////////// Méthode 2 //////////////////////////////////////////////////////////////////////
-        $task = $pdo->select("SELECT * FROM task WHERE id = " .$id);
-
+        
         echo $twig->render('taskshowpage2.twig',[
                                                     'task' => $task
                                                 ]);
--- src/repository/TaskRepository.php
+++ src/repository/TaskRepository.php
@@ -7,12 +7,53 @@ namespace App\TodolistPhp\Repository;
 use App\TodolistPhp\Services\Database;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
+use App\TodolistPhp\Services\Utils;
 
 // On déclare la class： TaskRepository
 class TaskRepository
 {
     public function index(){
 
+        // Se connecter à la base de données
+        $pdo = new Database (
+                                "127.0.0.1",
+                                "todolist_php",
+                                "3306",
+                                "root",
+                                ""
+                            );
+        $tasks = $pdo->selectAll("SELECT * FROM task order by id desc");
+        return $tasks;
+    }
+    public function new(){
+        
+            // Se connecter à la base de données
+        $pdo = new Database (
+                                "127.0.0.1",
+                                "todolist_php",
+                                "3306",
+                                "root",
+                                ""
+                            );
+    // Vérification des champs qui ont update dans le formulaire
+    if(isset($_POST['title']) && isset($_POST['description']) && isset($_POST['duration']) && isset($_POST['status'])
+    && !empty($_POST['title']) && !empty($_POST['description']) && !empty($_POST['duration']) && !empty($_POST['status'])) 
+        {
+            $title= Utils::cleaner($_POST['title']);
+            $description = Utils::cleaner($_POST['description']);
+            $duration = Utils::cleaner($_POST['duration']);
+            $status = Utils::cleaner($_POST['status']);
+
+            // Insertion des champs dans la base de données
+            $newTask = $pdo->query("    INSERT INTO task 
+                                        (title, description, duration, status) 
+                                        VALUES ( '$title', '$description', '$duration', '$status')");
+        }
+        return $newTask;
+    }
+    public function show($id){
+
+        // la correspondance de l'id souhaite via une requete sql
         // Se connecter à la base de données
         $pdo = new Database(
             "127.0.0.1",
@@ -21,8 +62,9 @@ class TaskRepository
             "root",
             ""
         );
-        $tasks = $pdo->selectAll("SELECT * FROM task order by id desc");
-        return $tasks;
+        $task = $pdo->select("SELECT * FROM task WHERE id = " .$id);
+        return $task;
     }
-}
 
+
+}
